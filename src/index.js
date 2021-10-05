00918b10d62d43fdc99af955daa54943a98e0e0b
JuniorMen <roinujstone22@outlook.com>
2021-10-04 20:44:23 -0500

    LoginPrueba

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+// (Auth0Provider) proveedor que almacenara la autenticacion de nuestros usuarios y proveera metodos para loguear y desloguerlos
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    
+    <Auth0Provider domain="dev-qems38v5.us.auth0.com" clientId="PPUhFNnQuDYpeK6njWRBBUoP60osxzK3" redirectUri={window.location.origin}>
+      <App />
+    </Auth0Provider>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
